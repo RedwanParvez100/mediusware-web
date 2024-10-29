1e05f8581a5e8352ea1ce14983fed73d968ee518
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2024-10-29 13:18:54 +0600

    navbar button added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <div className="banner py-44 text-center bg-slate-900">
+            <p className="text-8xl text-white font-bold">
+                Transforming Your Ideas <br />
+                into
+                <span className="text-green-500"> Digital Outcomes</span>
+            </p>
+            <br />
+            <p className="text-lg text-white">
+                Our global journey needs your expertise. Align with our brand
+                ethos and help us reach new heights across the world.
+            </p>
+            <button className="text-white mt-10 btn btn-info">
+                Talk to Someone
+            </button>
+        </div>
+    );
+};
+
+export default Banner;
